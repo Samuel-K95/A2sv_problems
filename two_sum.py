0ee020edb5e6843ea9fd64769760bfe5e62ba94a
Samuel Keno <sgere195@gmail.com>
2023-10-16 17:21:54 +0300

    twosum

---
+++ two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+
+        for i in range(len(nums)):
+            if nums[i] in store.values():
+                return (nums.index(target - nums[i]), i)
+            store.setdefault(i, target - nums[i])
