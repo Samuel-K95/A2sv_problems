36c33893a464f3210ba8f9affc7f8669c8d3445b
Samuel Keno <sgere195@gmail.com>
2023-10-30 13:33:00 +0300

    replace element

---
+++ replace_elem.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arrayChange(self, nums: list[int], operations: list[list[int]]) -> list[int]:
+        store = {}
+        for i in range(len(nums)):
+            store[nums[i]] = i
+        for first, second in operations:
+            ind = store[first]
+            del store[first]
+            store[second] = ind
+        st = list((y, k) for k, y in store.items())
+        st.sort(key=lambda v: v[0])
+        ans = []
+        for i in st:
+            ans.append(i[1])
+        return ans
