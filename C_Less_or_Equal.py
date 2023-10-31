bd09f378e69096dd7f56c3dea7ee555223d5fd64
Samuel Keno <sgere195@gmail.com>
2023-10-31 09:17:33 +0300

    less or equal

---
+++ C_Less_or_Equal.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+if k == 0:
+    if nums[0] == 1:
+        print(-1)
+    else:
+        print(1)
+else:
+    if k < len(nums):
+        if nums[k - 1] == nums[k]:
+            print(-1)
+        else:
+            print(nums[k - 1])
+    else:
+        print(nums[0])
