e9c61a5241fdeb6e287c93864bdd2b867950ca93
Samuel Keno <sgere195@gmail.com>
2023-11-13 21:04:14 +0300

    max number of k-sum pairs

---
+++ max_number_k_sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        nums.sort()
+        op = 0
+        while left < right:
+            if nums[left] + nums[right] < k:
+                left += 1
+            elif nums[left] + nums[right] > k:
+                right -= 1
+            else:
+                op += 1
+                left += 1
+                right -= 1
+        return op
