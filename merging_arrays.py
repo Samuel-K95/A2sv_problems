51f91afde58eb473b2e5c7e2e41d09482d65847a
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2023-11-14 15:13:32 +0300

    Update merging_arrays.py

--- merging_arrays.py
+++ merging_arrays.py
@@ -1,14 +1,22 @@
 n, m = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-x = 0
-ans = []
-for i in range(m):
-    try:
-        while a[x] < b[i]:
-            x += 1
-    except:
-        pass
-    ans.append(x)
-for i in ans:
+first = list(map(int, input().split()))
+second = list(map(int, input().split()))
+arr1 = 0
+arr2 = 0
+final = []
+for _ in range(n + m):
+    if arr1 < len(first) and arr2 < len(second):
+        if first[arr1] <= second[arr2]:
+            final.append(first[arr1])
+            arr1 += 1
+        else:
+            final.append(second[arr2])
+            arr2 += 1
+    else:
+        if arr1 < len(first):
+            final.extend(first[arr1:])
+        else:
+            final.extend(second[arr2:])
+        break
+for i in final:
     print(i, end=" ")
