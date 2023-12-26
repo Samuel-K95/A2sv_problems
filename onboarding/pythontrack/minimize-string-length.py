6aaaa2ef777202e5a522054987a91467ac6570ae
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2023-12-26 11:14:36 +0300

    Added solution for Minimize String Length

--- onboarding/pythontrack/minimize-string-length.py
+++ onboarding/pythontrack/minimize-string-length.py
@@ -1,30 +1,3 @@
 class Solution:
-    def delleft(self, i: int, s: list, t: str):
-        while i >= 0:
-            if s[i] == t:
-                return i
-            i -= 1
-        return -1
-
-    def delright(self, i: int, s:list, t:str):
-        while i < len(s):
-            if s[i] == t:
-                return i
-            i += 1
-        return -1
-
     def minimizedStringLength(self, s: str) -> int:
-        s=[i for i in s]
-        full = len(s)
-        for i in range(len(s)):
-            left = self.delleft(i-1, s, s[i])
-            right = self.delright(i+1, s, s[i])
-            if left != -1:
-                s[left] = ""
-                full -= 1
-            if right != -1:
-                s[right] = ""
-                full -= 1
-        s="".join(s)
-        print(s)
-        return len(s)        
+        return len(set(s))
