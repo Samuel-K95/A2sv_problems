b33ffb809aebbd1dcaf1c9444707e7e7ea8ded5b
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-01-11 17:49:42 +0300

    Added solution for 3Sum Closest

--- education/firstphase/3sum-closest.py
+++ education/firstphase/3sum-closest.py
@@ -1,11 +1,13 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        #similar approach with three sum but since the target can not be found from the sum  of the elements, we would store the smallest interval for each combination, and finally return it
+
+        #the curr pointer would iterate through the array and 
         nums.sort()
-        store = defaultdict(int)
+        pick, curr_coll = float('inf'), tuple()
         for curr in range(len(nums)):
             left = curr + 1
             right = len(nums) - 1
-            pick, curr_coll = float('inf'), tuple()
             while left < right:
                 current_sum = nums[curr] + nums[left] + nums[right]
                 if abs(target-current_sum) < pick:
@@ -15,8 +17,6 @@ class Solution:
                     left += 1
                 else:
                     right -= 1
-            store[curr_coll] = pick
-        store = sorted(store.items(), key = lambda x: x[1])
-        return sum(store[0][0])
+        return sum(curr_coll)
 
 
