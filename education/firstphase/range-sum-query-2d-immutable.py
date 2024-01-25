98e6c56856fbe07eb65fd137b749db13b431b433
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-01-25 18:09:45 +0300

    Added solution for Range Sum Query 2D - Immutable

--- education/firstphase/range-sum-query-2d-immutable.py
+++ education/firstphase/range-sum-query-2d-immutable.py
@@ -1,21 +1,18 @@
 class NumMatrix:
-    def __init__(self, matrix: list[list[int]]):
-        rows, cols = len(matrix), len(matrix[0])
-        # build ps 2D ps Table (PS)
-        self.ps = [[0] * (cols + 1) for _ in range(rows + 1)]
-        for r in range(1, rows + 1):
-            for c in range(1, cols + 1):
-                self.ps[r][c] = (
-                    self.ps[r][c - 1]
-                    + self.ps[r - 1][c]
-                    - self.ps[r - 1][c - 1]
-                    + matrix[r - 1][c - 1]
-                )
+
+    def __init__(self, matrix: List[List[int]]):
+        n = len(matrix)
+        m = len(matrix[0])
+        self.pref = [[0 for i in range(m + 1)] for j in range(n + 1)]
+        for r in range(1, len(matrix) + 1):
+            for c in range(1, len(matrix[0]) + 1):
+                self.pref[r][c] = matrix[r-1][c-1] + self.pref[r-1][c] + self.pref[r][c-1] - self.pref[r-1][c-1]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return (
-            self.ps[row2 + 1][col2 + 1]
-            - self.ps[row1][col2 + 1]
-            - self.ps[row2 + 1][col1]
-            + self.ps[row1][col1]
-        )
+        sum = self.pref[row2+1][col2+1] - self.pref[row1][col2 + 1] - self.pref[row2 + 1][col1] + self.pref[row1][col1]
+        return sum
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
