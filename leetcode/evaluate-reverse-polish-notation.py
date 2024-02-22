f25d72410a48a8987b9b58a7410466b9694e61c6
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-02-22 18:00:21 +0300

    Add solution for Evaluate Reverse Polish Notation

--- leetcode/evaluate-reverse-polish-notation.py
+++ leetcode/evaluate-reverse-polish-notation.py
@@ -1,12 +1,22 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        def calc(a, b , op):
+            if op == '+':
+                return str(int(int(a) + int(b)))
+            elif op == '-':
+                return str(int(int(a) - int(b)))
+            elif op == '*':
+                return str(int(int(a) * int(b)))
+            elif op == '/':
+                return str(int(int(a) / int(b)))
+
         stack = []
         ops = {'+', '/', '-', '*'}
         for i in range(len(tokens)):
             if tokens[i] in ops:
                 b = stack.pop()
                 a = stack.pop()
-                c = str(int(eval(a + tokens[i] + b)))
+                c = calc(a, b, tokens[i])
                 stack.append(c)
             else:
                 stack.append(tokens[i])
