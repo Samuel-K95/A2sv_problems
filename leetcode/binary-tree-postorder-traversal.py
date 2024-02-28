f95f4efdd426d42ac0973aa5153f7be3c3e34365
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-02-28 11:33:12 +0300

    Add solution for Binary Tree Postorder Traversal

---
+++ leetcode/binary-tree-postorder-traversal.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        store = []
+
+        def postOrder(node):
+            if not node:
+                return 
+
+            if node.left:
+                postOrder(node.left)
+
+            if node.right:
+                postOrder(node.right)
+            
+            store.append(node.val)
+        
+
+        postOrder(root)
+
+        return store
