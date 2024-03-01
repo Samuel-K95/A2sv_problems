3d8627fbaa6e1715c303d441d96ea72d63529398
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-03-01 15:47:59 +0300

    Add solution for Two City Scheduling

--- leetcode/two-city-scheduling.py
+++ leetcode/two-city-scheduling.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         costs.sort(key = lambda x: (x[0] - x[1]))
-        stack = []
+        stack = 0
         n = len(costs)
         for i in range(n):
             app = 0
-            if len(stack) >= n // 2:
+            if i >= n // 2:
                 app = 1
-            stack.append(costs[i][app])
+            stack  += costs[i][app]
         
-        return sum(stack)
+        return stack
