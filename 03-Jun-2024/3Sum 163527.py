fd2ed9bc28986290e3c9778c7f0a687dbf8064fb
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-06-03 17:00:33 +0300

    Created '3Sum' by A2SV Hub

---
+++ 03-Jun-2024/3Sum 163527.py
@@ -0,0 +1,27 @@
+# Problem: 3Sum - https://leetcode.com/problems/3sum/description/
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        c = set()
+        n = len(nums)
+        nums.sort()
+        for i in range(n):
+            l_most = nums[i]
+            left = i + 1
+            right = n - 1
+            while left < right:
+                if l_most + nums[left] + nums[right] > 0:
+                    right -= 1
+                elif l_most + nums[left] + nums[right] < 0:
+                    left += 1
+                else:
+                    curr = (l_most, nums[left], nums[right])
+                    if curr not in c:
+                        ans.append([l_most, nums[left], nums[right]])
+                        c.add(curr)
+                    left += 1
+                    right -= 1
+        return ans
+                    
+
