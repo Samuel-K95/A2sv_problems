bdf00936a5d37b8ee318948906b4661633698332
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-06-05 09:18:10 +0300

    Created 'Maximum Twin Sum of a Linked List' by A2SV Hub

---
+++ 05-Jun-2024/Maximum Twin Sum of a Linked List 164307.py
@@ -0,0 +1,32 @@
+# Problem: Maximum Twin Sum of a Linked List - https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+        
+        tr = 0
+        left = head
+        right = prev
+        while left and right:
+            tr = max(left.val + right.val, tr)
+            left = left.next
+            right = right.next
+        
+        return tr
+        
