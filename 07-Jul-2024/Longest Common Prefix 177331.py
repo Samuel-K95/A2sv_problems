55fac4a4cc42223db0a0f34e887c243210fca043
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-07-08 01:49:42 +0300

    Created 'Longest Common Prefix' by A2SV Hub

---
+++ 07-Jul-2024/Longest Common Prefix 177331.py
@@ -0,0 +1,22 @@
+# Problem: Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        def comp(a, b):
+            if len(a) > len(b):
+                return 1
+            else:
+                return -1
+
+        store = sorted(strs, key=cmp_to_key(comp))
+        ans = ""
+        if len(strs) == 1:
+            return strs[0]
+        wor = ""
+        for r in range(len(store[0])):
+            wor += store[0][r]
+            for j in range(1, len(store)):
+                if store[j][: r + 1] != wor:
+                    return ans
+            ans = wor
+        return ans
