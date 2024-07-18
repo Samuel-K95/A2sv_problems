b9f9d16d79383817d27550ba3dc5d23d5f3ea550
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-07-18 23:50:43 +0300

    Created 'Z-Sort' by A2SV Hub

---
+++ 18-Jul-2024/Z-Sort 181703.py
@@ -0,0 +1,28 @@
+# Problem: Z-Sort - https://codeforces.com/problemset/problem/652/B
+
+n = int(input())
+nums = list(map(int, input().split()))
+ 
+nums.sort()
+dup = [0] * n
+i = 0
+for j in range(0, len(nums), 2):
+    dup[j] = nums[i]
+    i += 1
+for j in range(1, len(nums), 2):
+    dup[j] = nums[i]
+    i += 1
+ 
+flag = True
+for j in range(1, len(nums), 2):
+    if dup[j] < dup[j - 1]:
+        flag = False
+        break
+for i in range(2, len(nums), 2):
+    if dup[i] > dup[i-1]:
+        flag = False
+        break
+if flag:
+    print(*dup)
+else:
+    print("Impossible")
