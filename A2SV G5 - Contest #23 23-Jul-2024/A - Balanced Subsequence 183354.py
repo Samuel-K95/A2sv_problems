62e31e8b6bcbcbcaa7f07dab4dbc6c85048ad86e
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-07-23 22:23:24 +0300

    Created 'A - Balanced Subsequence' by A2SV Hub

---
+++ A2SV G5 - Contest #23 23-Jul-2024/A - Balanced Subsequence 183354.py
@@ -0,0 +1,23 @@
+# Problem: A - Balanced Subsequence - https://codeforces.com/gym/532814/problem/A
+
+from collections import Counter
+
+
+n, k = map(int, input().split())
+
+s = input()
+
+track = Counter(s)
+
+small = float('inf')
+is_all = set()
+for i in track:
+    if ord(i) - 65 <= k:
+        small = min(small, track[i])
+        is_all.add(i)
+
+if len(is_all) == k:
+    print(small * k)
+else:
+    print(0)
+
