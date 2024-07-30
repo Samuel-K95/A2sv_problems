18d50bce104248c9eea95418a60870efd642f49a
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-07-31 00:23:20 +0300

    Created 'A - abbccc' by A2SV Hub

---
+++ A2SV G5 - Contest #21 30-Jul-2024/A - abbccc 186154.py
@@ -0,0 +1,11 @@
+# Problem: A - abbccc - https://codeforces.com/gym/530187/problem/A
+
+n = int(input())
+s = input()
+ans = []
+p, t = 0, 1
+while p < n:
+    ans.append(s[p])
+    p += t
+    t += 1
+print("".join(ans))
