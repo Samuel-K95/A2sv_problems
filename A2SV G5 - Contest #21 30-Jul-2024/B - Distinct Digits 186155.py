b5d417208e021fee48c82d1cf8a21c60f07c7eef
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-07-31 00:23:26 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 30-Jul-2024/B - Distinct Digits 186155.py
@@ -0,0 +1,22 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+t = int(input())
+def solve():
+    n = int(input())
+    s = 9 if n > 9 else n
+    ans = []
+    while n > 0:
+        ans.append(str(s))
+        n -= s
+        if n > 9:
+            s -= 1
+        else:
+            s = min(s-1, n)
+
+    ans.reverse()
+    print(int("".join(ans)))
+
+
+
+for _ in range(t):
+    solve()
