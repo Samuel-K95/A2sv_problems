ee83fa2836d3c4a8806b7b049edca864a0c5c1f7
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-08-04 01:14:48 +0300

    Created 'A - The Coordinate Challenge' by A2SV Hub

---
+++ A2SV G5 - Contest #22 03-Aug-2024/A - The Coordinate Challenge 187184.py
@@ -0,0 +1,24 @@
+# Problem: A - The Coordinate Challenge - https://codeforces.com/gym/531455/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    ans = 0
+    if n >= 3:
+        if n % 3 == 0:
+            ans = n // 3
+        else:
+            rem = (n // 3) * 3
+            n_rem = n - rem
+            if n_rem < 2:
+                ans = (n // 3) + 1
+            else:
+                ans = (n // 3) + 1
+    else:
+        if n == 1:
+            ans = 2
+        elif n == 2:
+            ans = 1
+            
+    print(ans)
+    
