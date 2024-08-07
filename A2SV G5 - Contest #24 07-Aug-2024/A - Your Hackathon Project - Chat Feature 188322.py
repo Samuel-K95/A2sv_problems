43880d798bfa3f31b82b024a611353d2c239953a
Samuel Keno <115112860+Samuel-K95@users.noreply.github.com>
2024-08-07 23:41:57 +0300

    Created 'A - Your Hackathon Project - Chat Feature' by A2SV Hub

---
+++ A2SV G5 - Contest #24 07-Aug-2024/A - Your Hackathon Project - Chat Feature 188322.py
@@ -0,0 +1,16 @@
+# Problem: A - Your Hackathon Project - Chat Feature - https://codeforces.com/gym/534160/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    cnt = 0
+    for i in range(n-1, -1, -1):
+        if s[i] != ')':
+            break
+        cnt += 1
+    rem = n - cnt
+    if cnt > rem:
+        print("Yes")
+    else:
+        print("No")
